72603d8f660618c12115beb670437c24510b6d47
Carlos Loura <carlos.loura@ingka.ikea.com>
2024-08-28 11:06:48 +0200

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,4 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Informação Ruas",
@@ -14,7 +11,7 @@ export default function RootLayout({ children }) {
       <head>
         <link href='https://fonts.googleapis.com/css?family=Crimson Pro' rel='stylesheet'/>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
